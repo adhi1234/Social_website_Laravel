f2a30d943798d13dabcb1de8e60d244e7e45eccf
Adarsh <astromediasnet16@gmail.com>
2016-07-04 15:28:40 +0530

    Zany Network™-Laravel Framework
    This is just a back-end structure of a social website made with trending
    version of  Laravel 5.2.

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,47 @@
+  @extends('layouts.master')
+
+  @section('title')
+WELCOME TO THE WORLD
+  @endsection
+
+@section('content')
+  <div class="row">
+    <div class="col-md-6">
+        <h3>Sign Up</h3>
+        <form action="{{ route('signup') }}" method="post">
+            <div class="form-group">
+              <label for="email">Your Email</label>
+              <input class="form-control" type="text" name="email" id="email">
+            </div>
+            <div class="form-group">
+              <label for="first_name">Your First Name</label>
+              <input class="form-control" type="text" name="first_name" id="first_name">
+            </div>
+            <div class="form-group">
+              <label for="password">Your Password</label>
+              <input class="form-control" type="password" name="password" id="password">
+            </div>
+
+            <button type="submit" class="btn btn-primary">Submit</button>
+            <input type="hidden" name="_token" value="{{ Session::token() }}">
+         </form>
+       </div>
+       <div class="col-md-6">
+         <h3>Sign In</h3>
+           <form action="{{route('signin') }}" method="post">
+               <div class="form-group">
+                 <label for="email">Your Email</label>
+                 <input class="form-control" type="text" name="email" id="email">
+               </div>
+
+               <div class="form-group">
+                 <label for="password">Your Password</label>
+                 <input class="form-control" type="password" name="password" id="password">
+               </div>
+               <button type="submit" class="btn btn-primary">Submit</button>
+<input type="hidden" name="_token" value="{{ Session::token() }}">
+            </form>
+          </div>
+  </div>
+
+@endsection
